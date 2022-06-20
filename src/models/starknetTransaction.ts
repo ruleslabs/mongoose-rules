9e9bd46936405c5fd512bbe3da713355589d846f
xshitaka <clanier.dev@gmail.com>
2022-06-20 16:48:14 +0200

    improved starknet transaction

--- src/models/starknetTransaction.ts
+++ src/models/starknetTransaction.ts
@@ -3,22 +3,13 @@ import { Document, model, Schema, Types } from 'mongoose'
 export interface StarknetTransaction {
   hash: string
   involvedUserIds?: Types.ObjectId[]
-  state?: number
-  type: number
+  syncing: boolean
+  failed: boolean
+  accepted: boolean
+  selector: string
+  initiatedByAddress: string
+  nonce: number
 }
-
-// state:
-//  -1 => failed
-//   0 => unsynced
-//   1 => sycing
-//   2 => synced
-
-// type:
-//  0 => deploy account
-//  1 => mint pack
-//  2 => transfer pack
-//  3 => mint card
-//  4 => transfer card
 
 export interface StarknetTransactionDocument extends StarknetTransaction, Document {}
 
@@ -35,24 +26,35 @@ const StarknetTransactionSchema = new Schema<StarknetTransactionDocument>({
     }],
     default: [],
   },
-  state: {
-    type: Number,
-    default: 0,
-    min: -1,
-    max: 2,
-    validate : {
-      validator : Number.isInteger,
-      message   : '{VALUE} is not an integer value',
-    },
+  syncing: {
+    type: Boolean,
+    required: true,
+    default: false,
   },
-  type: {
+  failed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  accepted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  selector: {
+    type: String,
+    required: true,
+  },
+  initiatedByAddress: {
+    type: String,
+  },
+  nonce: {
     type: Number,
     required: true,
-    min: 0,
-    max: 1,
-    validate : {
-      validator : Number.isInteger,
-      message   : '{VALUE} is not an integer value',
+    min: 1,
+    validate: {
+      validator: Number.isInteger,
+      message: 'nonce: {VALUE} is not an integer value',
     },
   },
 })

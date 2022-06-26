c3b9f3b5d83166458857cb52c198ef0a713c2523
xshitaka <clanier.dev@gmail.com>
2022-06-26 22:37:08 +0200

    capped settings and added pack minting nonce

--- src/models/settings.ts
+++ src/models/settings.ts
@@ -2,6 +2,7 @@ import { Document, model, Schema, Types } from 'mongoose'
 
 export interface Settings {
   lastBlockNumberSynced: number
+  packMintingNonce: number
 }
 
 export interface SettingsDocument extends Settings, Document {}
@@ -10,8 +11,15 @@ const SettingsSchema = new Schema<SettingsDocument>({
   lastBlockNumberSynced: {
     type: Number,
     min: 0,
-    required: true,
+    default: 0,
   },
+  packMintingNonce: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+}, {
+  capped: { size: 999999, max: 1 },
 })
 
 export const MongoSettings = model<SettingsDocument>('Settings', SettingsSchema, 'settings')
